c2bbbfdb7ca69bf955e9475c4dd6fc6416c1c027
TAAT <20purpleblue10@gmail.com>
2023-01-04 20:31:27 +0900

    Initial Commit

---
+++ YumemiCodeCheck/YumemiCodeCheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  YumemiCodeCheckApp.swift
+//  YumemiCodeCheck
+//
+//  Created by 董 亜飛 on 2023/01/04.
+//
+
+import SwiftUI
+
+@main
+struct YumemiCodeCheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
